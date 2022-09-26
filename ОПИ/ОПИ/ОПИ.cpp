fa5b5d8dc1ae0e4246cb5d4eaa397068d688eb61
pynikcmd <ver.r762@gmail.com>
2022-09-27 01:29:00 +0300

    Добавление строки Выполнила студентка

--- ОПИ/ОПИ/ОПИ.cpp
+++ ОПИ/ОПИ/ОПИ.cpp
@@ -6,9 +6,11 @@ int main()
     setlocale(LC_ALL, "Russian");
     cout << "Hello World!\n";
     cout << "Лабораторная работа №1\n";
+    cout << "Выполнила студентка\n";
     cout << "Вероника Трушева\n";
     cout << "ПИЖ-б-о-21-1\n";
     cout << "город Ставрополь\n";
     cout << "5 + 5 = " << 5 + 5 << "\n";
+    cout << ""
 
 }
