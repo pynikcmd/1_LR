1de76df91e691d8483034ede89070c6e752ea50d
pynikcmd <ver.r762@gmail.com>
2022-09-27 01:27:06 +0300

    Добавление строки Лабораторная работа №1

--- ОПИ/ОПИ/ОПИ.cpp
+++ ОПИ/ОПИ/ОПИ.cpp
@@ -5,8 +5,10 @@ int main()
 {
     setlocale(LC_ALL, "Russian");
     cout << "Hello World!\n";
+    cout << "Лабораторная работа №1\n";
     cout << "Вероника Трушева\n";
     cout << "ПИЖ-б-о-21-1\n";
     cout << "город Ставрополь\n";
-    cout << "5 + 5 = " << 5 + 5;
+    cout << "5 + 5 = " << 5 + 5 << "\n";
+
 }
