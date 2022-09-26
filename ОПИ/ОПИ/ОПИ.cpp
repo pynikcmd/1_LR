63a8942702e41905c57573552c57289a74e7f814
pynikcmd <ver.r762@gmail.com>
2022-09-27 01:22:24 +0300

    Добавление строки город Ставрополь

--- ОПИ/ОПИ/ОПИ.cpp
+++ ОПИ/ОПИ/ОПИ.cpp
@@ -3,7 +3,9 @@ using namespace std;
 
 int main()
 {
+    setlocale(LC_ALL, "Russian");
     cout << "Hello World!\n";
-    cout << "Вероника Трушева";
-    cout << "ПИЖ-б-о-21-1";
+    cout << "Вероника Трушева\n";
+    cout << "ПИЖ-б-о-21-1\n";
+    cout << "город Ставрополь\n";
 }
